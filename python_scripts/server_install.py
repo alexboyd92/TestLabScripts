64a80f6482d7455881a12ba1de27199a04c74417
theKerrigan <33499973+theKerrigan@users.noreply.github.com>
2020-03-15 16:23:15 -0400

    Add files via upload

---
+++ python_scripts/server_install.py
@@ -0,0 +1,24 @@
+def runCommand(bashCommand):
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+	return 0
+# updates/ upgrades all programs
+def update():
+	update = "sudo apt-get update -y"
+	upgrade = "sudo apt-get upgrade -y"
+	runCommand(update)
+	print ("update complete")
+	runCommand(upgrade)
+	print ("upgrade complete")
+	return 0
+
+def  main():
+	update()
+	exec(open('gvminstall.py').read())
+
+
+
+	return 0	
+
+if __name__ == '__main__':
+	main()
